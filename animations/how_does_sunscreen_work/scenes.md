e1b4f75757deed37560a4b2b4892068e1f8ae372
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2023-06-17 18:24:01 +0200

    Work in progress

---
+++ animations/how_does_sunscreen_work/scenes.md
@@ -0,0 +1,108 @@
+# Escenas
+
+## Escena 1
+
+### Texto
+Llega el verano a este nuestro querido hemisferio norte y con él las tradicinales consignas veraniegas: Bebe agua cada poco tiempo, no te metas al mar justo después de comer y ponte crema solar. Mucha crema solar.
+
+### Desarrollo
+1. El matraz está echado en una tumbona bajo una sombrilla, con gafas de sol, medio rojo a trozos y chorreando sudor. De fondo hay una playa, una pelota, castillos de arena y gaviotas. El agua del mar entra y sale de la costa.
+
+2. Cuando se listan las consignas aparecen dentro de círculos:
+    * Una botella
+    * Un estómago dentro de un prohibido
+    * Crema solar con un sol negro dentro
+
+    Se ponen a dar vueltas y, cuando dice mucha crema solar se hace zoom a tope al solecito hasta que la pantalla se vuelve negra.
+
+## Escena 2
+
+### Texto
+
+Desde el Sol nos llega a la Tierra un amplio espectro de radiación electromagnética:
+
+* De 3800 a 700 nm tenemos radiación infrarroja: calorcico que nos manda el Sol.
+* De 700 a alrededor de 400 tenemos la luz visible que compone nuestros queridos arcoiris. 
+* Entre los 400 y los 100 nm tenemos a la RUV. Típicamente, separamos a esta RUV en 3 tipos: UVA, de 400 a 320 nm; UVB, de 320 a 280 nm y UVC, de 280 a 100 nm. 
+
+La radiación UVC es la más peligrosa, pero por suerte la capa de ozono absorbe la mayoría. Sin embargo, la UVA y sobretodo la UVB restantes son las responsable de hacer que nos pongamos morenicos pero también de las quemaduras, el fotoenvejecimiento y el cáncer de piel entre otros males. 
+
+### Desarrollo
+
+1. Con la pantalla en negro, se generan los ejes de un gráfico y se grafica la radiación solar directa. 
+2. Para cada fragmento, se demarca con líneas los límites de cada región y se rellenan de algún color. 
+    * Para la infrarroja, puede ser un color rojo que pasa a gris conforme va hacia la derecha.
+    * Visible se ponen colores del arcoiris y 1 segundo de lo suponía.
+    * Para la ultravioleta algo parecido a la del infrarrojo pero de violeta a negro y demarcando cada una de las zonas. 
+3. Cuando hablemos de los daños de la RUV el gráfico va a la derecha y a la izquierda se listan varios problemas que causa.
+
+
+## Escena 4
+
+### Texto
+
+¡No preocuparse! La química está aquí para ayudarnos a diseñar cremas solares que nos ayuden a protegernos de la RUV.
+
+Los fotoprotectores se clasifican en inorgánicos particulados y en orgánicos. Dentro de los orgánicos, tenemos los tradicionales, los poliméricos y los particulados. Si nos vamos a su estructura química, podemos clasificarlos de otra forma: 
+
+* Derivados de PABA
+* Cinamatos
+* Oxybenzonas
+* Salicilatos
+* Dibenzoilmetanos
+* Óxidos inorgánicos
+* Otros
+
+Esta clasificación viene de las moléculas base que originan a las demás. Si te fijas, muchas de ellas son idénticas pero con algunos fragmentos modificados.
+
+### Desarrollo
+
+1. El matraz hace aparición y pone pose de mostrar bíceps. 
+2. Dibujar todas las estructuras aprobadas por la UE y ponerlas en sus posiciones iniciales.
+3. Borrar las etiquetas de las categorías principales y reordenar según su clasificación química.
+4. Cuando estén en su sitio, hacer aparecer la molécula base.
+5. Remarcar en color las zonas que son idénticas y luego las zonas que son distintas para cada molécula y su molécula base. 
+
+
+## Escena 5
+
+### Texto
+Independientemente de su estructura, la mayoría de fotoprotectores absorber la RUV para proteger nuestra piel. ¿Como funciona eso?
+
+Comencemos con un átomo. Esta es la representación de Bohr de un átomo y, aunque no es la más adecuada, messirve para ejemplificar. En el centro tenemos el núcleo atómico, compuesto por protones y electrones. A su alrededor, tenemos electrones girando en órbitas similares a las de los planetas que giran alrededor del Sol. En lugar de órbitas vamos a llamarlos orbitales atómicos. DISCLAMER: LOS ORBITALES ATÓMICOS NO TIENEN ESTA PINTA, PERO ME SIRVEN PARA LA EXPLICACIÓN. Los electrones solo pueden moverse dentro de esos orbitales, pero pueden pasar de un orbital a otro si absorben o emiten una cantidad adecuada de energía en forma de fotones. Los fotones son las partículas asociadas a la radiación electromagnética, incluyendo a la radiación ultravioleta. ¿Ves por donde vamos?
+
+### Desarrollo 
+
+1. FadeOut a todo.
+2. Diagrama de Bohr de un átomo de carbono con algunos niveles.
+3. Pausar la animación y poner el disclaimer con un efecto como de tele rota o señal parada. 
+4. Electrón sube al recibir un fotón y baja al emitirlo en bucle
+
+
+## Escena 6
+
+### Texto
+En las moléculas tenemos átomos unidos entre sí mediante enlaces. Estos enlaces se pueden entender como una versión extendida de los orbitales atómicos en las que en lugar de uno hay dos o más núcleos atómicos. Además, podemos encadenar, o mejor dicho, conjugar estos orbitales para producir orbitales moleculares de una energía específica. Por ejemplo, el beta-caroteno tiene un sistema conjugado que cae en el rango de la luz naranja, lo cual le da su característico color a las zanahorias.
+
+Los fotoprotectores usan sistemas conjugados para conseguir que la energía de sus orbitales moleculares se encuentre en un rango capaz de absorber radiación ultravioleta
+
+### Desarrollo
+
+1. FadeOut a todo. 
+2. Diagrama de Lewis del H2
+3. Orbital molecular sigma enlazante del H2 y luego el antienlazante. Un electrón pasa de un nivel a otro.
+4. FadeOut y luego FadeIn del betacaroteno. Si puede ser con un un brillo o algo así mejor para que se vea más chulo.
+5. Lo mismo pero con la Avobenzona. 
+
+
+## Escena 7
+
+### Texto
+
+¡Alto el carro! Si la energía de los fotones es absorbida por la molécula y luego liberada en forma de otro fotón con la misma energía, al final la RUV nos alcanza igualmente, ¿verdad?
+
+Bueno, ese es uno de los mecanismos de pérdida de energía, pero hay más. Por ejemplo, existe la fotofragmentación: La molécula utiliza la energía absorbida para romperse. Otros seguro que te suenan, como la fluorescencia que puedes observar en algunos subrayadores; o la fosforescencia, utilizada habitualmente en las señales de salida de emergencia para que sean visibles cuando no haya luz.
+
+La mayoría de fotoprotectores utiliza un mecanismo conocido como relajación vibracional, consistente en disipar la energía absorbida en forma de energía térmica.
+
+### Desarrollo
---
+++ animations/how_does_sunscreen_work/test_file.py
@@ -0,0 +1,187 @@
+import pandas as pd
+from manim import *
+
+COLOR_0 = "#24283B"
+COLOR_A = "#F7768E"
+COLOR_B = "#9ECE6A"
+COLOR_C = "#E0AF68"
+COLOR_D = "#7AA2F7"
+COLOR_E = "#BB9AF7"
+COLOR_F = "#7DCFFF"
+COLOR_GRAY = "#8B93B5"
+
+config.background_color = COLOR_0
+class SunData(Scene):
+    def construct(self):
+        data = pd.read_csv("solar_radiation.csv")
+        wavelenght = data["lambda"].to_numpy()
+        etr_solar = data["etr"].to_numpy()
+        direct_solar = data["direct"].to_numpy()
+        
+        
+        big_axis = Axes(
+            x_range = (100, 4000, 300),
+            x_axis_config={"include_numbers": True, "font_size": 15},
+            y_range=(0,2.5),
+            tips=False,
+            color=COLOR_GRAY
+        )
+        big_axis2 = big_axis.copy()
+        small_axis = Axes(
+            x_range=(100, 750, 50),
+            x_axis_config={"include_numbers": True, "font_size": 15},
+            y_range=(0,2.5),
+            tips=False,
+            color=COLOR_GRAY
+        )
+        uv_axis = Axes(
+            x_range=(100, 400, 20),
+            x_axis_config={"include_numbers": True, "font_size": 15},
+            y_range=(0,1.5),
+            tips=False,
+            color=COLOR_GRAY
+        )
+        
+        etr_data = big_axis.plot_line_graph(
+            x_values=wavelenght,
+            y_values=etr_solar,
+            line_color=COLOR_C,
+            stroke_width=1
+         )["line_graph"]
+        
+        etr_data2 = etr_data.copy()
+        etr_data_small = small_axis.plot_line_graph(
+            x_values=wavelenght,
+            y_values=etr_solar,
+            line_color=COLOR_C,
+            stroke_width=1
+        )["line_graph"]
+        
+        direct_uv_data = uv_axis.plot_line_graph(
+            x_values=wavelenght,
+            y_values=etr_solar,
+            line_color=COLOR_C,
+            stroke_width=1
+            
+        )["line_graph"]
+        
+        direct_solar_data = big_axis2.plot_line_graph(
+            x_values=wavelenght,
+            y_values=direct_solar,
+            line_color=COLOR_D,
+            stroke_width=1
+        )["line_graph"]
+        
+        
+        self.play(Create(big_axis), run_time=1.5)
+        self.play(Create(etr_data), run_time=3)
+        
+        self.wait(2)
+        
+
+        def get_intersection_updater(no_added_mob, background, gradient, gradient_index):
+            def updater(added_mob):
+                added_mob.become(Intersection(no_added_mob, background, sheen_direction=RIGHT, fill_opacity=0.5, stroke_width=0).set_color(color_gradient(gradient, gradient_index)))
+            return updater
+        
+        infrared_region = Rectangle(width=10.15, height=6).shift(0.93*RIGHT)
+        infrared_mask = Square().scale(10).set_opacity(0).next_to(infrared_region, LEFT, buff=0)
+        infrared_rectangle = VMobject().add_updater(get_intersection_updater(infrared_region, infrared_mask, [RED, RED_C, RED_B, RED_A], 4))
+        
+        self.add(infrared_rectangle)
+        self.play(infrared_mask.animate.shift(infrared_region.get_width()*RIGHT), run_time=4)
+        
+        self.wait()
+        self.play(infrared_mask.animate.shift((infrared_region.get_width()+1)*LEFT), run_time=2)
+        
+        self.wait()
+        
+        
+        self.play(
+           ReplacementTransform(big_axis, small_axis),
+           ReplacementTransform(etr_data, etr_data_small),
+           run_time=3
+        )
+        
+        self.wait()
+        
+        visible_region = Rectangle(width=7.2, height=6).shift(1.2*RIGHT)
+        visible_mask = Square().scale(10).set_opacity(0).next_to(visible_region, LEFT, buff=0)
+        visible_rectangle = VMobject().add_updater(get_intersection_updater(visible_region, visible_mask, [PURPLE, BLUE, TEAL, GREEN, YELLOW, ORANGE, RED], 4))
+        
+        
+        self.add(visible_rectangle)
+        self.play(visible_mask.animate.shift(visible_region.get_width()*RIGHT), run_time=4)
+        
+        self.wait()
+        self.play(visible_mask.animate.shift((visible_region.get_width()+1)*LEFT), run_time=2)
+        
+        self.wait()
+        
+        self.play(
+            ReplacementTransform(small_axis, uv_axis),
+            ReplacementTransform(etr_data_small, direct_uv_data),
+            run_time=3
+        )
+        
+        self.wait()
+        
+        uv_region = Rectangle(width=12, height=6)
+        uv_mask = Square().scale(10).set_opacity(0).next_to(uv_region, LEFT, buff=0)
+        uv_rectangle = VMobject().add_updater(get_intersection_updater(uv_region, uv_mask, [BLACK, PURPLE_E, PURPLE_D, PURPLE_C], 4))
+        
+        self.add(uv_rectangle)
+        self.play(uv_mask.animate.shift(uv_region.get_width()*RIGHT), run_time=4)
+        
+        self.wait()
+        
+        uva_rectangle = Rectangle(width=3.2, height=6).shift(4.4*RIGHT)
+        uvb_rectangle = Rectangle(width=1.6, height=6).shift(2*RIGHT)
+        uvc_rectangle = Rectangle(width=7.2, height=6).shift(2.4*LEFT)
+        
+        uva_inter = Intersection(direct_uv_data, uva_rectangle, fill_opacity=1, stroke_width=0, color=PURPLE_A)
+        uvb_inter = Intersection(direct_uv_data, uvb_rectangle, fill_opacity=1, stroke_width=0, color=PURPLE_C)
+        uvc_inter = Intersection(direct_uv_data, uvc_rectangle, fill_opacity=1, stroke_width=0, color=PURPLE_E)
+        
+        uva = Text("UVA", stroke_color=COLOR_0, stroke_width=1, fill_opacity=1, fill_color=LIGHT_PINK).scale(0.8).move_to([4.5, -2, 0])
+        uvb = Text("UVB", stroke_color=COLOR_0, stroke_width=1, fill_opacity=1, fill_color=LIGHT_PINK).scale(0.8).move_to([2.1, -2, 0])
+        uvc = Text("UVC", stroke_color=COLOR_0, stroke_width=1, fill_opacity=1, fill_color=LIGHT_PINK).scale(0.8).move_to([-0.5, -2, 0])
+        
+        self.play(
+            AnimationGroup(
+                FadeIn(uva_inter),
+                FadeIn(uvb_inter),
+                FadeIn(uvc_inter),
+            ),
+            AnimationGroup(
+                FadeIn(uva),
+                FadeIn(uvb),
+                FadeIn(uvc),
+            )
+        )
+        
+        self.wait()
+        
+        self.play(
+            FadeOut(uva),
+            FadeOut(uvb),
+            FadeOut(uvc),
+        )
+        self.play(
+            FadeOut(uva_inter),
+            FadeOut(uvb_inter),
+            FadeOut(uvc_inter),
+        )
+        
+        self.play(uv_mask.animate.shift((uv_region.get_width()+1)*LEFT), run_time=2)
+        self.wait()
+        
+        self.play(
+            ReplacementTransform(uv_axis, big_axis2),
+            ReplacementTransform(direct_uv_data, etr_data2),
+            run_time=3
+        )
+        
+        self.play(Create(direct_solar_data), run_time=3)
+        
+        self.wait()
